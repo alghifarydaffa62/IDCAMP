35adaca4c13ce7587acb0e1fa143dc76265c6fc2
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-11-05 10:49:14 +0700

    contacts app

---
+++ belajar-vite/contacts-app/src/App.jsx
@@ -0,0 +1,13 @@
+import ContactApp from './component/ContactApp'
+import './style/style.css'
+
+function App() {
+    return(
+        <div>
+            <ContactApp/>
+        </div>
+        
+    )
+}
+
+export default App
--- belajar-vite/contacts-app/src/index.jsx
+++ belajar-vite/contacts-app/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
- 
-const element = <h1>Hello, world!</h1>;
- 
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<App/>);
