d2ea7e831373ba7f225b594c853825b7ac807945
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-11-06 17:35:58 +0700

    state

--- belajar-vite/contacts-app/src/App.jsx
+++ belajar-vite/contacts-app/src/App.jsx
@@ -1,10 +1,12 @@
 import ContactApp from './component/ContactApp'
+import CounterApp from './component/CounterApp'
 import './style/style.css'
 
 function App() {
     return(
         <div>
             <ContactApp/>
+            <CounterApp/>
         </div>
         
     )
