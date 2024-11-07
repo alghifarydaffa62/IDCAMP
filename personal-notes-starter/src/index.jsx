3c56b28762eda9da98ec45c8be00033c43ad0f48
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-11-07 13:31:25 +0700

    starting

--- personal-notes-starter/src/index.jsx
+++ personal-notes-starter/src/index.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-// import style
-import './styles/style.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>);
+const root = createRoot(document.getElementById('root'))
+root.render(<App/>)
