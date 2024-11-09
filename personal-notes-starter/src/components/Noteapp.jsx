43f01805609afdce31876710fec5f678cb854b56
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-11-09 12:25:52 +0700

    data

--- personal-notes-starter/src/components/Noteapp.jsx
+++ personal-notes-starter/src/components/Noteapp.jsx
@@ -1,8 +1,17 @@
 import React from "react"
 import NoteInput from "./NoteInput"
+import { getInitialData } from "../utils"
 import '../styles/style.css'
 
 class NoteApp extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            note: getInitialData()
+        }
+    }
     render() {
         return(
             <div className="note-app__body">
